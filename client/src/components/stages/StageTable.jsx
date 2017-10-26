de5094ec7fdd2272d07ad33e3fc9fa11ff9035e6
Vladimir Dralo <vladra@users.noreply.github.com>
2017-10-26 18:03:26 +0300

    --wip-- [skip ci]

--- client/src/components/stages/StageTable.jsx
+++ client/src/components/stages/StageTable.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { graphql } from 'react-apollo';
 import { Link, withRouter } from 'react-router-dom';
-import { groupBy, max, sumBy, times } from 'lodash';
+import { groupBy, max, minBy, sumBy, times } from 'lodash';
 
 import { GetStage } from 'gql/queries';
 import { Table } from 'components/Table';
@@ -17,11 +17,19 @@ const prepareRacers = ({ racers, laps }) =>
 
     return {
       ...r,
+      showOnGraph: true,
       laps: racerLaps,
       totalLaps: racerLaps.count,
       totalTime: sumBy(racerLaps, 'time'),
+      fastestLap: minBy(racerLaps, 'time'),
     };
   });
+
+const prepareStage = ({ stage, laps }) => {
+  const fastestLap = minBy(laps, 'time');
+
+  return { ...stage, fastestLap };
+};
 
 const findRacer = ({ racers, racerId }) =>
   racers.find(r => r.id === racerId);
@@ -35,7 +43,7 @@ const timeToHuman = (time) => {
 };
 
 // Generate Columns
-const generateLapsColumns = (racers) => {
+const generateLapsColumns = ({ stage, racers }) => {
   const total = max(racers.map(r => (r.laps || []).length));
 
   return times(total, (i) => {
@@ -48,21 +56,46 @@ const generateLapsColumns = (racers) => {
         const lap = findRacer({ racers, racerId }).laps.find(l => l.number === lapNumber);
         return lap ? lap.h_time : '';
       },
+      Cell: ({ original: { id: racerId, fastestLap }, row }) => { // eslint-disable-line
+        let style = {};
+        const lapTime = row[`lap-${lapNumber}`];
+        console.log(stage.fastestLap.number, lapNumber, racerId, stage.fastestLap.racer_id)
+        if (stage.fastestLap.number === lapNumber && racerId === stage.fastestLap.racer_id) {
+          style = { color: 'green' };
+        } else if (fastestLap.number === i + 1) {
+          style = { color: 'red' };
+        }
+
+        return <span style={style}>{lapTime}</span>;
+      },
     };
   });
 };
 
-const generateColumns = racers => [{
+const generateColumns = ({ stage, racers }) => [{
+  Header: '',
+  accessor: 'showOnGraph',
+  // eslint-disable-next-line
+  Cell: ({ original: { showOnGraph } }) => <input type="checkbox" checked={showOnGraph} />,
+}, {
   Header: 'Имя',
   accessor: 'name',
-}, ...generateLapsColumns(racers), {
+}, {
+  Header: 'Номер',
+  accessor: 'number',
+}, ...generateLapsColumns({ stage, racers }), {
   id: 'totalTime',
   Header: 'Общее время',
-  accessor: ({ id: racerId }) => sumBy(findRacer({ racers, racerId }), 'time'),
+  accessor: ({ id: racerId }) => {
+    const { laps } = findRacer({ racers, racerId });
+    return { totalLaps: laps.length, totalTime: sumBy(laps, 'time') };
+  },
   Cell: ({ original: { id: racerId } }) => timeToHuman(findRacer({ racers, racerId }).totalTime),
   sortMethod: (a, b) => {
-    console.log(a, b)
-    return 0
+    if (a.totalLaps === b.totalLaps) {
+      return a.totalTime < b.totalTime ? 1 : -1;
+    }
+    return a.totalLaps < b.totalLaps ? -1 : 1;
   },
 }];
 
@@ -71,9 +104,19 @@ const mapResultToProps = ({ data }) => {
   const { loading } = data;
   if (!data.stage) return { loading };
 
-  const { stage } = data;
+  const { stage: { racers, laps, ...stage } } = data;
 
-  return { stage, loading };
+  const preparedStage = prepareStage({ stage, laps });
+  const preparedRacers = prepareRacers({ racers, laps });
+  const klasses = racers.map(r => r.klass);
+
+  return {
+    stage: preparedStage,
+    racers: preparedRacers,
+    laps,
+    klasses,
+    loading,
+  };
 };
 
 const mapPropsToOptions = ({ match }) => ({
@@ -81,15 +124,14 @@ const mapPropsToOptions = ({ match }) => ({
 });
 
 
-const StagesIndexTable = ({ stage, loading }) => {
+const StagesIndexTable = ({ stage, racers, laps, klasses, loading }) => {
   if (loading) return null;
-
-  const racers = prepareRacers(stage);
 
   return (
     <Table
-      columns={generateColumns(racers)}
+      columns={generateColumns({ stage, racers })}
       data={racers}
+      defaultSorted={[{ id: 'totalTime', desc: true }]}
     />
   );
 };
